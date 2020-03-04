1936fc53a5ac758d77cce28f2d9ef28b58eef48b
yorkmass <1458510486@qq.com>
2020-03-04 19:34:04 +0800

    test

--- src/main/java/com/yorkmass/demo/mapper/MyUserMapper.java
+++ src/main/java/com/yorkmass/demo/mapper/MyUserMapper.java
@@ -6,9 +6,12 @@ import org.apache.ibatis.annotations.Select;
 import java.util.Map;
 
 public interface MyUserMapper {
+    /*
+    通过用户名获取密码
+     */
     @Select("select password from user where username=#{username}")
     String getPasswordByUsername(String username);
-
+    
     @Select("SELECT roles.rolename FROM `user` INNER JOIN roles ON `user`.roleid = roles.roleid WHERE `user`.username = #{username}")
     String getRolebyUsername(String username);
 
