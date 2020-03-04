866601b942b3cf38ff8cc2aafec497e4e2923524
yorkmass <1458510486@qq.com>
2020-03-04 19:37:13 +0800

    "test2"

--- src/main/java/com/yorkmass/demo/mapper/MyUserMapper.java
+++ src/main/java/com/yorkmass/demo/mapper/MyUserMapper.java
@@ -11,10 +11,14 @@ public interface MyUserMapper {
      */
     @Select("select password from user where username=#{username}")
     String getPasswordByUsername(String username);
-    
+    /*
+    通过用户名获得角色名
+     */
     @Select("SELECT roles.rolename FROM `user` INNER JOIN roles ON `user`.roleid = roles.roleid WHERE `user`.username = #{username}")
     String getRolebyUsername(String username);
-
+    /*
+    通过用户名获取用户
+     */
     @Select("select * from user where username=#{username}")
     User getUserByUsername(String username);
 
